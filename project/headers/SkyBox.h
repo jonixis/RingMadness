6d8937d8de5477b16ac1b7d9365b13303e3439e0
Jonathan Stahl <joni@stahlfamily.ch>
2018-04-13 11:51:36 +0200

    init skybox class

---
+++ project/headers/SkyBox.h
@@ -0,0 +1,16 @@
+//
+//  SkyBox.h
+//  bRenderer_ios
+//
+//  Created by Jonathan Stahl on 13.04.18.
+//
+
+#ifndef SkyBox_h
+#define SkyBox_h
+
+class SkyBox {
+public:
+    void init();
+};
+
+#endif /* SkyBox_h */
